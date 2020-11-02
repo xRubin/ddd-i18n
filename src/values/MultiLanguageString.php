e213f5b36ea578930f33ffc4d3e0d0b87b9244b7
xRubin <dalamar84@mail.ru>
2020-11-02 19:56:19 +0300

    initial commit

---
+++ src/values/MultiLanguageString.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace ddd\i18n\values;
+
+use ddd\domain\values\AbstractDomainValue;
+
+class MultiLanguageString extends AbstractDomainValue
+{
+    /** @var string[] */
+    private array $value = [];
+    private ?string $default = null;
+
+    /**
+     * MultiLanguageString constructor.
+     * @param array|string[] $values
+     * @param string|null $default
+     */
+    public function __construct(array $values, ?string $default = null)
+    {
+        $this->value = $values;
+        $this->default = $default;
+    }
+
+    public function getStringForLanguage(string $language): ?string
+    {
+        if (array_key_exists($language, $this->value))
+            return $this->value[$language];
+
+        return $this->default;
+    }
+
+    public function setForLanguage(string $language, ?string $value): self
+    {
+        $this->value[$language] = $value;
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getValue(): array
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDefault(): ?string
+    {
+        return $this->default;
+    }
+}
